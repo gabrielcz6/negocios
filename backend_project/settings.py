f1fdca462a6f1f642766b48c3b2d3f4d45f44bab
gabrielcanepa <gabrielcanepamercado@gmail.com>
2024-12-02 09:19:24 -0500

    first commit

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -27,7 +27,8 @@ MEDIA_ROOT = BASE_DIR / 'media'  # Esto ahora debería funcionar correctamente
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r_4_760j(nmn=9*2l2%(f7iieo2(^(h^c=xxn)@%x3kh3q*q*q'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
